09fd44db223119aa55ee09dff28f9fff2a8a8ce6
RadSpaceEd <radspaceed@kalli.st>
2023-12-02 17:13:50 -0300

    Revert "Realized I accidentally transfered the "highlight on yank" funtion to the ftplugin/markdown.lua file"
    This reverts commit ea118ba3e07119488d5ebdd87321f5a78b5983c8.

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -12,3 +12,14 @@ vim.keymap.set('n', '<Down>', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent
 
 -- Quickly align table 
 vim.keymap.set('n', '<leader>mt', 'vip:!pandoc -t markdown-simple_tables<CR>', { silent = true , desc = 'Align the table with pandoc'})
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
